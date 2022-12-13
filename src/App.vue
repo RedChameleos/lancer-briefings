03d79d57e848f015546c067676e60b7093bf0a62
Redchameleos <Redch@WINDOWS-STUULL7>
2022-12-13 20:27:07 +0000

    size?

--- src/App.vue
+++ src/App.vue
@@ -133,6 +133,14 @@ export default {
           "frame": "Everest",
           "mech": "Graveyard Shift"
         },
+        {
+          "callsign": "Phantasm",
+          "alias": 'Leyna Charlotte Sophie Jordan Erxleben VII',
+          "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
+          "corpro": "GMS",
+          "frame": "Everest",
+          "mech": "Graveyard Shift"
+        },
       ],
       "header": {
         "planet": "Hercynia",
@@ -204,8 +212,8 @@ export default {
 
 <style lang="scss">
 #app {
-  width: 1902px;
-  height: 910px;
+  width: 100%;
+  height: 100%;
   overflow: hidden;
 }
 </style>
